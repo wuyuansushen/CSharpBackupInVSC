4d61998c54ae82921945fef0f866bf62a840cf79
wuyuansushen <wuyuansushen@gmail.com>
2020-11-28 19:13:50 +0800

    asNulaable Tutorial

---
+++ asNullable/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace asNullable
+{
+    class Program
+    {
+        static void PrintValueAsInt32(object o)
+        {
+            int? nullable=o as int?;
+            Console.WriteLine(nullable.HasValue?nullable.Value.ToString():"null");
+        }
+        static void Main(string[] args)
+        {
+            PrintValueAsInt32(5);
+            PrintValueAsInt32("string");
+        }
+    }
+}
