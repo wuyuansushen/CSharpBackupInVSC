6a5901f4b47df7120319666ae41b5f65e4f06cb5
wuyuansushen <wuyuansushen@gmail.com>
2020-11-29 20:24:45 +0800

    Assembly attributes

---
+++ attributeInternalsVisible/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+[assembly:InternalsVisibleTo("")]
+namespace attributeInternalsVisible
+{
+//[assembly:InternalsVisibleTo("")]
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
