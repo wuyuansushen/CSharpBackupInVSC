513f90dc5287bfd66d577f857bc53e20685d050f
wuyuansushen <wuyuansushen@gmail.com>
2020-11-29 14:36:49 +0800

    IteratorTutorial

--- finallyUsing/Program.cs
+++ finallyUsing/Program.cs
@@ -1,12 +1,31 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 
 namespace finallyUsing
 {
     class Program
     {
+        static IEnumerable<string> Iterator()
+        {
+            try{
+                Console.WriteLine("Before first yield");
+                yield return "first";
+                Console.WriteLine("Between yields");
+                yield return "second";
+                Console.WriteLine("After second yield");
+            }
+            finally
+            {
+                Console.WriteLine("In finally block");
+            }
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            foreach(string value in Iterator())
+            {
+                Console.WriteLine($"Received value: {value}");
+            }
         }
     }
 }
