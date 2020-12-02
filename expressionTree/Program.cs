43bf4c77a75ced686b4088bca7afb5d72cfff475
wuyuansushen <wuyuansushen@gmail.com>
2020-12-02 13:54:15 +0800

    Resource Lambda Tree expression

---
+++ expressionTree/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq.Expressions;
+
+namespace expressionTree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ParameterExpression xParameter=Expression.Parameter(typeof(int),"x");
+            ParameterExpression yParameter=Expression.Parameter(typeof(int),"y");
+            Expression body=Expression.Add(xParameter,yParameter);
+            ParameterExpression[] parameters=new[] {xParameter,yParameter};
+            Expression<Func<int,int,int>> addr=Expression.Lambda<Func<int,int,int>>(body,parameters);
+            Console.WriteLine(addr);
+        }
+    }
+}
