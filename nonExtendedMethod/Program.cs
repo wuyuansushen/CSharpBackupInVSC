484f677e0dae9b6d8b79979e90958897d8d6a1b8
wuyuansushen <wuyuansushen@gmail.com>
2020-12-04 14:45:29 +0800

    Extended Method

---
+++ nonExtendedMethod/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace nonExtendedMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] words={"keys","coat","laptop","bottle"};
+            IEnumerable<string> query=Enumerable.Select(Enumerable.OrderBy(Enumerable.Where(words,word=>word.Length>4),word=>word),word=>word.ToUpper());
+            foreach(var value in query)
+            {
+                Console.WriteLine(value);
+            }
+        }
+    }
+}
---
+++ simpleQuery/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace simpleQuery
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] words={"keys","coat","laptop","bottle"};
+            IEnumerable<string> query=words.Where(word=>word.Length>4).OrderBy(word=>word).Select(word=>word.ToUpper());
+            foreach(string value in query)
+            {
+                Console.WriteLine(value);
+            }
+        }
+    }
+}
