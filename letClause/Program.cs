5c8cfe2e5b792066961f80ff921994863902f5fc
wuyuansushen <wuyuansushen@gmail.com>
2020-12-04 15:22:19 +0800

    let clause

---
+++ letClause/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace queryExpression
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] words={"abandon","deprecated","generic","query","hi","Guys"};
+            IEnumerable<string> query=from word in words
+            let leng=word.Length
+            where leng > 4
+            orderby word
+            select word.ToUpper();
+            foreach(var value in query)
+            {
+                Console.WriteLine(value);
+            }
+        }
+    }
+}
