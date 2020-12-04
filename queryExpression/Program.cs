86cea71851a67fb3377752f2e8fd3ef8487bb15e
wuyuansushen <wuyuansushen@gmail.com>
2020-12-04 15:13:29 +0800

    linq

---
+++ queryExpression/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace queryExpression
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] words={"abandon","deprecated","generic","query","hi","Guys"};
+            IEnumerable<string> query=from word in words
+            where word.Length>4
+            orderby word
+            select word.ToUpper();
+            foreach(var value in query)
+            {
+                Console.WriteLine(value);
+            }
+        }
+    }
+}
